66cb6cd4a4d78c9c8ebac8e01da4ebefd482f151
fdhhhdjd <nguyentientai10@gmail.com>
2023-08-27 13:00:39 +0700

    #1 feature: bitmap

--- Redis/bitmap/bitmap.js
+++ Redis/bitmap/bitmap.js
@@ -5,23 +5,30 @@ const redis = new Redis({
     host: 'localhost',
 });
 
-const userOnlineKey = 'user:online:20230827'; // Tên khóa bitmap cho người dùng trực tuyến ngày 2023-08-27
+// key name  for user online date 2023-08-27
+// Tên khóa bitmap cho người dùng trực tuyến ngày 2023-08-27
+const userOnlineKey = 'user:online:20230827';
 
+// User markup online at position 1 in bitmap
+// Đánh dấu người dùng đang hoạt động (bit ở vị trí 0) trong bitmap
 async function markUserOnline(userId) {
     await redis.setbit(userOnlineKey, userId, 1);
 }
 
+// User markup offline at position 0 in bitmap
 // Đánh dấu người dùng vắng mặt (bit ở vị trí 0) trong bitmap
 async function markUserOffline(userId) {
     await redis.setbit(userOnlineKey, userId, 0);
 }
 
+// Check user have off or online in bitmap
 // Kiểm tra xem người dùng có mặt hay vắng mặt trong bitmap
 async function checkUserStatus(userId) {
     const isOnline = await redis.getbit(userOnlineKey, userId);
     return isOnline === 1 ? 'Online' : 'Offline';
 }
 
+// Count user online in bitmap ( note: bitcount only count bitmap equal 1)
 // Đếm số lượng người dùng online từ bitmap 1
 async function countUsersOnline() {
     const count = await redis.bitcount(userOnlineKey);
@@ -36,11 +43,16 @@ const handleRunCheck = async () => {
     await markUserOffline(91011);
     await markUserOffline(111);
 
-    console.info('User 123:', await checkUserStatus(123)); // Xuất ra "Offline"
-    console.info('User 456:', await checkUserStatus(456)); // Xuất ra "Online"
-    console.info('User 789:', await checkUserStatus(456)); // Xuất ra "Online"
-    console.info('User 91011:', await checkUserStatus(91011)); // Xuất ra "Offline"
-    console.info('User 111:', await checkUserStatus(111)); // Xuất ra "Online"
+    // Print "Offline" or "Online"
+    console.info('User 123:', await checkUserStatus(123));
+
+    console.info('User 456:', await checkUserStatus(456));
+
+    console.info('User 789:', await checkUserStatus(456));
+
+    console.info('User 91011:', await checkUserStatus(91011));
+
+    console.info('User 111:', await checkUserStatus(111));
 
     console.info('Total users online:', await countUsersOnline());
 
